b1b02c7f379b269e31668da8754c35edce76625d
kchang00 <k_chang@fanshaweonline.ca>
2019-11-01 16:51:07 -0400

    added lightbox with bio data functionality

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,61 @@
 const myVM = (() => {
     // get the user buttons and fire off an async DB query with Fetch
 
+    let userButtons = document.querySelectorAll('.u-link'),
+        lightBox = document.querySelector('.lightbox');
+
+    
+    //create the social media list
+    function renderSocialMedia(media) {
+        return `<ul class="u-social">
+                    ${media.map(item => `<li>${item}</li>`).join('')}
+                </ul>
+                `
+    }
+
+    function parseUserData(person) {
+        let targetDiv = lightBox.querySelector('.lb-content');
+        let targetImg = lightBox.querySelector('img');
+
+        let bioContent =  `
+            <p>${person.bio}</p>
+            <h4>Social Media: </h4>
+
+            <!-- loop through social media stuff here -->
+            ${renderSocialMedia(person.social)}
+        `;
+
+        targetDiv.innerHTML = bioContent;
+        targetImg.src = person.currentSrc;
+
+        lightBox.classList.add('show-lb');
+
+    }
+
+    function getUserData(event) {
+        // preventing browser from calling up new url, it doesn't exist
+        event.preventDefault();
+        // debugger;
+        // href= 1, 2, 3 depending on which anchor tag you click
+        let url = `/${this.getAttribute('href')}`;
+           let currentImg = this.previousElementSibling.getAttribute('src');
+        //called a promise
+        //fetches database content (or an API endpoint)
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                // take the existing img src from the layout (person) and use it in lightbox
+                data.currentSrc = currentImg;
+                parseUserData(data);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    userButtons.forEach(button => button.addEventListener('click', getUserData));
+    // make the lightbox close btn work
+    lightBox.querySelector('.close').addEventListener('click', function() {
+        lightBox.classList.remove('show-lb');
+    });
+
 })();
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const sql = require('../utils/sql');
 
+//.get = super global array
 // req = request, res = response
-// req: you're saying 'give me everything that goes with this route'
+// req: you're saying 'give me the data that goes with this route'
 // res: server says 'ok', packages the data up, and gives it back to the browser
 
-//.get = super global array
-
+//THIS ROUTE IS THE HOME
 router.get('/', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
@@ -25,14 +25,14 @@ router.get('/', (req, res) => {
         // render the home view with dynamic data
 
         // can change the key (changeme: result) to whatever you want
-        // controls 'people' var on home.hbs
+        // controls 'person' var on home.hbs
         res.render('home', { person: result });
     })
 })
 
-// can name id anything ('/:nameme')
+//THIS IS THE OTHER PAGES
+// can name id anything ('/:nameme') = parameter, what the user passes through
 router.get('/:id', (req, res) => {
-    // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the user route');
     console.log(req.params.id); // look at the request, get the parameter, fetch whatever the id is
 
@@ -44,11 +44,24 @@ router.get('/:id', (req, res) => {
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
-        // render the home view with dynamic data
+        console.log(result);
+
+        //convert the social property into an array before sending it through
+        //map is an array method that lets you map one value to another (convert it)
+        //grabbing commas and splitting them before and after to select 3 different values
+        result[0].social = result[0].social.split(',').map(function(item){
+
+             //removes any empty white space from text
+            item = item.trim();
+
+            return item;
+        })
+
+        console.log('after trim / conversion:', result[0]);
 
         // can change the key (changeme: result) to whatever you want
         // controls 'people' var on home.hbs
-        res.render('home', { person: result });
+        res.json(result[0]);
     })
 })
 
